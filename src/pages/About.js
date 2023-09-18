b05a1f98626b2ae0c27c8545c9172f230a9a5838
enzo-le-gouallec <131043823+enzo-le-gouallec@users.noreply.github.com>
2023-09-18 08:27:39 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae sed consequuntur quo iure ut minima quod officia consectetur accusantium, ratione reiciendis voluptatum ipsam recusandae tempora fugit harum repellendus ipsa natus quisquam maxime nostrum non magnam maiores! Aspernatur provident ullam nisi totam, necessitatibus nihil dignissimos velit quod magnam nam vitae perspiciatis, veniam vel ipsum sed corporis? Corporis perspiciatis saepe quas harum repudiandae soluta odit quae aperiam corrupti! Sapiente, unde? Neque officiis est, facere eligendi quia consequatur sed, quibusdam quos fugiat quidem temporibus esse eius optio reiciendis hic maxime! Quidem fugit dolorem molestias, architecto mollitia tenetur laborum impedit, error officia atque iste?</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptate nesciunt! Vel exercitationem quas quasi obcaecati consequatur cum ea quia eligendi, labore illo soluta doloribus enim, excepturi sequi accusamus magni modi consequuntur ad nam accusantium voluptatem quisquam qui deserunt. Repellat ex accusantium suscipit esse culpa saepe sapiente. Eaque blanditiis molestias facere ipsum ipsa amet minus. Quaerat eveniet amet reiciendis dolorum facilis omnis eligendi animi officia fugit, perspiciatis exercitationem minus laudantium ut nobis corporis enim quod id laboriosam nulla dignissimos! Quis reiciendis quos molestiae, esse culpa vel? Rem cum inventore libero consequatur suscipit, sapiente quo ipsa laudantium corrupti. Voluptates ad ut libero excepturi voluptas ratione quas rerum, alias quia commodi perferendis at quaerat rem tenetur odit animi, aperiam corporis vel. Quod vitae corrupti ut sunt voluptates! Sit explicabo nulla ipsam eos dolor odio quidem animi, quo magnam ab optio natus delectus fugiat deleniti corporis molestiae labore nostrum. Nemo accusamus alias iure atque veritatis hic sunt tempore nisi. Laboriosam minus recusandae, ab tempore commodi fuga accusamus? Neque esse dolores itaque reprehenderit tenetur reiciendis iure, cum, illo dolore adipisci aliquam voluptatum porro nostrum necessitatibus ipsam asperiores consequatur! Eius unde, odit animi, harum assumenda nam architecto illum aliquam inventore nobis veritatis alias, quis sapiente?</p>
+
+        </div>
+    );
+};
+
+export default About;
